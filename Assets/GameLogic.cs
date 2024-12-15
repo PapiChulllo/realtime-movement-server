e3083288a72612f6dca7160ce130a98ec1b931f1
Melih Burak <122559884+PapiChulllo@users.noreply.github.com>
2024-12-14 21:29:49 -0500

    Circle Click is removed and player movement added

--- Assets/GameLogic.cs
+++ Assets/GameLogic.cs
@@ -3,57 +3,31 @@ using UnityEngine;
 
 public class GameLogic : MonoBehaviour
 {
-    private Dictionary<int, Vector2> balloonPositions = new Dictionary<int, Vector2>();
-    private int nextBalloonID = 0;
+    private Dictionary<int, Vector2> playerPositions = new Dictionary<int, Vector2>();
 
     void Start()
     {
         NetworkServerProcessing.SetGameLogic(this);
     }
 
-    void Update()
+    public void UpdatePlayerPosition(int playerId, Vector2 newPosition)
     {
-        if (Time.time % 1 < Time.deltaTime) // Spawn a balloon every second
-        {
-            SpawnBalloon();
-        }
-    }
+        playerPositions[playerId] = newPosition;
 
-    private void SpawnBalloon()
-    {
-        float screenPositionXPercent = UnityEngine.Random.Range(0.0f, 1.0f);
-        float screenPositionYPercent = UnityEngine.Random.Range(0.0f, 1.0f);
-        Vector2 screenPosition = new Vector2(screenPositionXPercent * Screen.width, screenPositionYPercent * Screen.height);
-
-        int balloonID = nextBalloonID++;
-        balloonPositions[balloonID] = screenPosition;
-
-        string message = $"{(int)ServerToClientSignifiers.SpawnBalloon},{balloonID},{screenPosition.x},{screenPosition.y}";
+        // Notify all clients about the player's new position
+        string message = $"PlayerMoved,{playerId},{newPosition.x},{newPosition.y}";
         foreach (var connectionId in NetworkServerProcessing.GetAllClientIDs())
         {
             NetworkServerProcessing.SendMessageToClient(message, connectionId, TransportPipeline.ReliableAndInOrder);
         }
     }
 
-    public void BalloonPopped(int balloonID, int clientID)
+    public void SendAllPlayerPositions(int clientId)
     {
-        if (!balloonPositions.ContainsKey(balloonID)) return;
-
-        balloonPositions.Remove(balloonID);
-
-        string message = $"{(int)ServerToClientSignifiers.BalloonPopped},{balloonID}";
-        foreach (var connectionId in NetworkServerProcessing.GetAllClientIDs())
+        foreach (var player in playerPositions)
         {
-            NetworkServerProcessing.SendMessageToClient(message, connectionId, TransportPipeline.ReliableAndInOrder);
-        }
-    }
-
-    public void SendUnpoppedBalloons(int clientID)
-    {
-        foreach (var balloon in balloonPositions)
-        {
-            string message = $"{(int)ServerToClientSignifiers.SpawnBalloon},{balloon.Key},{balloon.Value.x},{balloon.Value.y}";
-            NetworkServerProcessing.SendMessageToClient(message, clientID, TransportPipeline.ReliableAndInOrder);
+            string message = $"PlayerMoved,{player.Key},{player.Value.x},{player.Value.y}";
+            NetworkServerProcessing.SendMessageToClient(message, clientId, TransportPipeline.ReliableAndInOrder);
         }
     }
 }
--- Assets/NetworkServerProcessing.cs
+++ Assets/NetworkServerProcessing.cs
@@ -1,17 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum ClientToServerSignifiers
-{
-    BalloonPopped = 1
-}
-
-public enum ServerToClientSignifiers
-{
-    SpawnBalloon = 1,
-    BalloonPopped = 2
-}
-
 static public class NetworkServerProcessing
 {
     static NetworkServer networkServer;
@@ -20,12 +9,13 @@ static public class NetworkServerProcessing
     static public void ReceivedMessageFromClient(string msg, int clientConnectionID, TransportPipeline pipeline)
     {
         string[] csv = msg.Split(',');
-        int signifier = int.Parse(csv[0]);
 
-        if (signifier == (int)ClientToServerSignifiers.BalloonPopped)
+        if (csv[0] == "PlayerMoved")
         {
-            int balloonID = int.Parse(csv[1]);
-            gameLogic.BalloonPopped(balloonID, clientConnectionID);
+            int playerId = clientConnectionID;
+            float x = float.Parse(csv[1]);
+            float y = float.Parse(csv[2]);
+            gameLogic.UpdatePlayerPosition(playerId, new Vector2(x, y));
         }
     }
 
@@ -41,23 +31,18 @@ static public class NetworkServerProcessing
 
     static public void ConnectionEvent(int clientConnectionID)
     {
-        UnityEngine.Debug.Log("Client connected with ID: " + clientConnectionID);
-        gameLogic.SendUnpoppedBalloons(clientConnectionID);
+        UnityEngine.Debug.Log($"Client with ID {clientConnectionID} connected.");
+        gameLogic.SendAllPlayerPositions(clientConnectionID);
     }
 
     static public void DisconnectionEvent(int clientConnectionID)
     {
-        UnityEngine.Debug.Log("Client disconnected with ID: " + clientConnectionID);
+        UnityEngine.Debug.Log($"Client with ID {clientConnectionID} disconnected.");
     }
 
     static public void SetNetworkServer(NetworkServer NetworkServer)
     {
         networkServer = NetworkServer;
-    }
-
-    static public NetworkServer GetNetworkServer()
-    {
-        return networkServer;
     }
 
     static public void SetGameLogic(GameLogic GameLogic)
